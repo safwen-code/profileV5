882999f210c9971f365c9eaf08e763f2c854fd11
safwen-code <safwendjebbi1234@gmail.com>
2024-05-27 16:51:50 +0100

    fadiiiiiiit ya zebbi

--- src/content/skills/AnimatedProgressProvider.jsx
+++ src/content/skills/AnimatedProgressProvider.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 const AnimatedProgressProvider = ({ interval = 1000, values, children }) => {
   const [valuesIndex, setValuesIndex] = useState(0)
