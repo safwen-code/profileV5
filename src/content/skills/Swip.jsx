03fc65e1276f133f620fa69be7af8b70529481b0
safwen-code <safwendjebbi1234@gmail.com>
2024-05-29 16:23:21 +0100

    update Swip v1.0

--- src/content/skills/Swip.jsx
+++ src/content/skills/Swip.jsx
@@ -4,36 +4,35 @@ import Box from '@mui/material/Box'
 import MobileStepper from '@mui/material/MobileStepper'
 import Paper from '@mui/material/Paper'
 import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft'
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight'
 import SwipeableViews from 'react-swipeable-views'
 import { autoPlay } from 'react-swipeable-views-utils'
 import { Grid, Card } from '@mui/material'
 import { motion } from 'framer-motion'
 
+import ang from '../../image/ang.jpg'
+import react from '../../image/react.png'
+import vue from '../../image/vue.png'
+import nodejs from '../../image/nodejs.png'
+import express from '../../image/express.png'
+import php from '../../image/php.png'
+import mongodb from '../../image/mongodb.png'
+import mysql from '../../image/mysql.png'
+import graph from '../../image/graph.jpg'
+
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews)
 
 const images = [
   {
-    label: 'San Francisco – Oakland Bay Bridge, United States',
-    imgPath:
-      'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+    label: 'Framework I Use',
+    imgPath: [ang, react, vue],
   },
   {
-    label: 'Bird',
-    imgPath:
-      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
+    label: 'backend Envirement I Use',
+    imgPath: [nodejs, php, express],
   },
   {
-    label: 'Bali, Indonesia',
-    imgPath:
-      'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
-  },
-  {
-    label: 'Goč, Serbia',
-    imgPath:
-      'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
+    label: 'Database I Use',
+    imgPath: [mongodb, mysql, graph],
   },
 ]
 
@@ -46,15 +45,6 @@ function SwipeableTextMobileStepper() {
   const theme = useTheme()
   const [activeStep, setActiveStep] = React.useState(0)
   const maxSteps = images.length
-
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1)
-  }
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1)
-  }
-
   const handleStepChange = (step) => {
     setActiveStep(step)
   }
@@ -84,48 +74,22 @@ function SwipeableTextMobileStepper() {
           <div key={step.label}>
             {Math.abs(activeStep - index) <= 2 ? (
               <Grid container spacing={6} mt={4} mb={4}>
-                <Grid item xs={12} sm={6} md={4}>
-                  <motion.div variants={gridItemVariants}>
-                    <Card
-                      sx={{
-                        height: 170,
-                        maxWidth: 345,
-                        background: `url(${step.imgPath})`,
-                        backgroundSize: 'cover',
-                        backgroundPosition: 'center',
-                        opacity: '0.9',
-                      }}
-                    ></Card>
-                  </motion.div>
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                  <motion.div variants={gridItemVariants}>
-                    <Card
-                      sx={{
-                        height: 170,
-                        maxWidth: 345,
-                        background: `url(${step.imgPath})`,
-                        backgroundSize: 'cover',
-                        backgroundPosition: 'center',
-                        opacity: '0.9',
-                      }}
-                    ></Card>
-                  </motion.div>
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                  <motion.div variants={gridItemVariants}>
-                    <Card
-                      sx={{
-                        height: 170,
-                        maxWidth: 345,
-                        background: `url(${step.imgPath})`,
-                        backgroundSize: 'cover',
-                        backgroundPosition: 'center',
-                        opacity: '0.9',
-                      }}
-                    ></Card>
-                  </motion.div>
-                </Grid>
+                {step.imgPath.map((img, index) => (
+                  <Grid item xs={12} sm={6} md={4}>
+                    <motion.div variants={gridItemVariants}>
+                      <Card
+                        sx={{
+                          height: 170,
+                          maxWidth: 345,
+                          background: `url(${img})`,
+                          backgroundSize: 'cover',
+                          backgroundPosition: 'center',
+                          opacity: '0.9',
+                        }}
+                      ></Card>
+                    </motion.div>
+                  </Grid>
+                ))}
               </Grid>
             ) : null}
           </div>
