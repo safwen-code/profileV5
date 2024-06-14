36a4cd7af60e4aa48f903e75db0b42c68a1c4bd2
safwen-code <safwendjebbi1234@gmail.com>
2024-06-14 21:00:26 +0100

    update Swip

--- src/content/skills/Swip.jsx
+++ src/content/skills/Swip.jsx
@@ -6,7 +6,7 @@ import Paper from '@mui/material/Paper'
 import Typography from '@mui/material/Typography'
 import SwipeableViews from 'react-swipeable-views'
 import { autoPlay } from 'react-swipeable-views-utils'
-import { Grid, Card } from '@mui/material'
+import { Grid, Card, useMediaQuery } from '@mui/material'
 import { motion } from 'framer-motion'
 
 import ang from '../../image/ang.jpg'
@@ -49,6 +49,22 @@ function SwipeableTextMobileStepper() {
     setActiveStep(step)
   }
 
+  let showScroll = useMediaQuery(theme.breakpoints.down('md'))
+  const imagescrol = [
+    {
+      label: 'Frameworks ',
+      imgPath: react,
+    },
+    {
+      label: ' Envirement that I Use',
+      imgPath: nodejs,
+    },
+    {
+      label: 'Database I Use',
+      imgPath: mongodb,
+    },
+  ]
+
   return (
     <Box sx={{ flexGrow: 1 }} m={1}>
       <Paper
@@ -72,66 +88,111 @@ function SwipeableTextMobileStepper() {
             },
           }}
         >
-          {images[activeStep].label}
+          {!showScroll
+            ? images[activeStep].label
+            : imagescrol[activeStep].label}
         </Typography>
       </Paper>
-      <AutoPlaySwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-        index={activeStep}
-        onChangeIndex={handleStepChange}
-        enableMouseEvents
-      >
-        {images.map((step, index) => (
-          <Grid
-            key={step.label}
-            sx={{
-              marginLeft: '50px',
-              [theme.breakpoints.down('md')]: {
-                marginLeft: '2px',
-              },
-            }}
+      {!showScroll ? (
+        <AutoPlaySwipeableViews
+          axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+          index={activeStep}
+          onChangeIndex={handleStepChange}
+          enableMouseEvents
+        >
+          {images.map((step, index) => (
+            <Grid
+              key={step.label}
+              sx={{
+                marginLeft: '50px',
+                [theme.breakpoints.down('md')]: {
+                  marginLeft: '2px',
+                },
+              }}
+            >
+              {Math.abs(activeStep - index) <= 2 ? (
+                <Grid
+                  container
+                  spacing={3}
+                  mt={0.1}
+                  mb={4}
+                  sx={{
+                    flexDirection: { xs: 'column', sm: 'row' },
+                  }}
+                >
+                  {step.imgPath.map((img, index) => (
+                    <Grid
+                      item
+                      xs={12}
+                      sm={6}
+                      md={4}
+                      key={index}
+                      sx={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                      }}
+                    >
+                      <motion.div variants={gridItemVariants}>
+                        <Card
+                          sx={{
+                            height: { xs: '120px', md: '120px', sm: '60px' },
+                            width: { xs: '140px', md: '160px', sm: '120px' },
+                            background: `url(${img})`,
+                            backgroundSize: 'cover',
+                            backgroundPosition: 'center',
+                          }}
+                        ></Card>
+                      </motion.div>
+                    </Grid>
+                  ))}
+                </Grid>
+              ) : null}
+            </Grid>
+          ))}
+        </AutoPlaySwipeableViews>
+      ) : (
+        <>
+          <AutoPlaySwipeableViews
+            axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+            index={activeStep}
+            onChangeIndex={handleStepChange}
+            enableMouseEvents
           >
-            {Math.abs(activeStep - index) <= 2 ? (
-              <Grid
-                container
-                spacing={3}
-                mt={0.1}
-                mb={4}
-                sx={{
-                  flexDirection: { xs: 'column', sm: 'row' },
-                }}
-              >
-                {step.imgPath.map((img, index) => (
+            {imagescrol.map((step, index) => (
+              <Grid key={step.label}>
+                {Math.abs(activeStep - index) <= 2 ? (
                   <Grid
-                    item
-                    xs={12}
-                    sm={6}
-                    md={4}
-                    key={index}
+                    container
                     sx={{
                       display: 'flex',
                       justifyContent: 'center',
                       alignItems: 'center',
                     }}
+                    spacing={3}
+                    mt={0.1}
+                    mb={4}
                   >
-                    <motion.div variants={gridItemVariants}>
-                      <Card
-                        sx={{
-                          height: { xs: '120px', md: '120px', sm: '60px' },
-                          width: { xs: '140px', md: '160px', sm: '120px' },
-                          background: `url(${img})`,
-                          backgroundSize: 'cover',
-                          backgroundPosition: 'center',
-                        }}
-                      ></Card>
-                    </motion.div>
+                    <Grid item key={index}>
+                      <motion.div variants={gridItemVariants}>
+                        <Card
+                          sx={{
+                            height: '100px',
+                            width: '100px',
+                            background: `url(${step.imgPath})`,
+                            backgroundSize: 'cover',
+                            backgroundPosition: 'center',
+                          }}
+                        ></Card>
+                      </motion.div>
+                    </Grid>
                   </Grid>
-                ))}
+                ) : null}
               </Grid>
-            ) : null}
-          </Grid>
-        ))}
-      </AutoPlaySwipeableViews>
+            ))}
+          </AutoPlaySwipeableViews>
+        </>
+      )}
       <Box
         sx={{
           display: 'flex',
